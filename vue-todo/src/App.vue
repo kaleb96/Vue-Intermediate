197b2bcbc1447034b87dc33270a13f488bf81e84
kaleb96 <yes505304@gmail.com>
2023-03-14 20:05:55 +0900

    TodoHeader
    Simple Styling TodoHeader

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -26,5 +26,21 @@ export default {
 </script>
 
 <style>
+  body {
+    text-align: center;
+    background-color: #F6F6F6;
+  }
 
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+
+  button {
+    border-style: groove;
+  }
+
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
+  }
 </style>
--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        header
-    </div>
+   <header>
+        <h1>ToDo it!</h1>
+   </header>
 </template>
 
-<script>
-export default {
+<style scoped>
+  
+  h1 {
+    color: #2F3852;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
 
-}
-</script>
-
-<style>
-
+  
 </style>
