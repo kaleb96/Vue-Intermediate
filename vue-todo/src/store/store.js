1c26b14f97dbb561cd5edf0e34c5c68fd441e25e
kaleb96 <yes505304@gmail.com>
2023-09-06 17:23:26 +0900

    update
    update code with helper

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -30,7 +30,11 @@ export const store = new Vuex.Store({
         // headerText: 'TODO it!',
         todoItems: storage.fetch()
     },
-
+    getters: {
+        storedTodoItems(state) {
+            return state.todoItems;
+        }
+    },
     mutations: {
         addOneItem(state, todoItem) {
             const obj = {completed: false, item: todoItem};
