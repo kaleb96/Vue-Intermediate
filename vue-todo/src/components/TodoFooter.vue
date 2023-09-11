4758937347e91d8347c808de8454ee4da56e990f
kaleb96 <yes505304@gmail.com>
2023-09-11 17:19:42 +0900

    Modified
    Modified Error
    - Store.js 에서 modules 오타 확인 후 수정 (moduels -> modules)

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -5,30 +5,28 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations } from 'vuex'
 
 export default {
-
   methods: {
     ...mapMutations({
       clearTodo: 'clearAllItems'
-    }),
+    })
   }
 }
 </script>
 
 <style scoped>
-  .clearAllContainer {
-    width: 8.5rem;
-    height: 50px;
-    line-height: 50px;
-    background-color: white;
-    border-radius: 5px;
-    margin: 0 auto;
-  }
-
-  .clearAllBtn {
-    color: #e20303;
-    display: block;
-  }
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
 </style>
